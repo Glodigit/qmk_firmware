e210673b4c8bd5199a30057bb64087761d34c70b
Glodigit <kdengelin@hotmail.com>
2023-10-18 19:07:53 +0100

    adjusted files fou taipo update

--- keyboards/lets_split/keymaps/Glodigit/config.h
+++ keyboards/lets_split/keymaps/Glodigit/config.h
@@ -21,8 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 //#define ONESHOT_TAP_TOGGLE
 
-#define TAIPO_ENABLE
 #define TAIPO_FLAVOR_POSH
+#define TAIPO_TAP_TIMEOUT 150
+#define ONESHOT_TIMEOUT 500
 
 #undef  DEBOUNCING_DELAY //gateron clears are too sensitive
 #define DEBOUNCING_DELAY 36
+
+#define FORCE_NKRO
--- keyboards/lets_split/keymaps/Glodigit/keymap.c
+++ keyboards/lets_split/keymaps/Glodigit/keymap.c
@@ -1,13 +1,9 @@
 #include QMK_KEYBOARD_H
-#include "taipo.h"
+#include "dlip.h"
 
 
-//Glodigit Default Keymap v1.0
+//Glodigit Keymap
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BASE   0
 #define _TAIPO  1
 #define _FN 5
